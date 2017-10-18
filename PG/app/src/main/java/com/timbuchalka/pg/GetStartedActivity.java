ad49b876319c09116113e9fe8b4aa5b564716371
jbr5433 <31827506+jbr5433@users.noreply.github.com>
2017-10-17 22:36:25 -0400

    Added functionality to drug spinner

--- PG/app/src/main/java/com/timbuchalka/pg/GetStartedActivity.java
+++ PG/app/src/main/java/com/timbuchalka/pg/GetStartedActivity.java
@@ -1,10 +1,16 @@
 package com.timbuchalka.pg;
 
+import android.database.DataSetObserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
-public class GetStartedActivity extends AppCompatActivity {
+public class GetStartedActivity extends AppCompatActivity implements SpinnerAdapter{
     private static final String TAG = "GetStartedActivity";
 
     @Override
@@ -12,5 +18,69 @@ public class GetStartedActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate==========================================");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
+
+        Spinner spinner = (Spinner) findViewById(R.id.drug_spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.drugs_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public void registerDataSetObserver(DataSetObserver observer) {
+
+    }
+
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver observer) {
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return 0;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
     }
 }
